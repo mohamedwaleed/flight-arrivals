56d7b92737a1148743a8ddecbd35ee4d8767a528
Mohamed Wlaeed <mohamedwlaeed2012@gmail.com>
2017-11-04 23:10:08 +0200

    modify grunt file for github pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,12 @@ module.exports = function (grunt) {
   // Define the configuration for all the tasks
   grunt.initConfig({
     buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
       pages: {
         options: {
           remote: 'git@github.com/mohamedwaleed/flight-arrivals.git',
