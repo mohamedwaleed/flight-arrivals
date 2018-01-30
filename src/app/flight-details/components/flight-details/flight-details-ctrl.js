1bd7667993f3eaeb7a7307493360de4c96e905ac
Mohamed Wlaeed <mohamedwlaeed2012@gmail.com>
2018-01-30 16:05:05 +0200

    change chart to linechart

--- src/app/flight-details/components/flight-details/flight-details-ctrl.js
+++ src/app/flight-details/components/flight-details/flight-details-ctrl.js
@@ -6,95 +6,93 @@ import {
 } from '../../../selectors/search-flights-selector';
 
 class FlightDetailsCtrl {
-    constructor($log, $stateParams, flightService, $ngRedux, $scope) {
-     'ngInject';
-     if(!$stateParams.origin || !$stateParams.destination) {
-        // $state.go('app.dashboard');
-        // return;
-     }
-     this.origin = $stateParams.origin;
-     this.dest = $stateParams.destination;
-
-     this.$log = $log;
-     this.origin = $stateParams.origin;
-     this.dest = $stateParams.destination;
-     this.flightService = flightService;
-     this.options = {
-        chart: {
-            type: 'historicalBarChart',
-            height: 400,
-            margin : {
-                top: 20,
-                right: 20,
-                bottom: 65,
-                left: 50
-            },
-            x: function(d){return d[0];},
-            y: function(d){return d[1];},
-            showValues: true,
-            duration: 100,
-            xAxis: {
-                axisLabel: 'X Axis',
-                rotateLabels: 30,
-                showMaxMin: false
-            },
-            yAxis: {
-                axisLabel: 'Y Axis',
-                axisLabelDistance: -10,
-            },
-            tooltip: {
-                keyFormatter: function(d) {
-                    return d3.time.format('%x')(new Date(d));
-                }
-            },
-            zoom: {
-                enabled: true,
-                scaleExtent: [1, 10],
-                useFixedDomain: false,
-                useNiceScale: false,
-                horizontalOff: false,
-                verticalOff: true,
-                unzoomEventType: 'dblclick.zoom'
-            }
+    constructor($log, $stateParams, flightService, $ngRedux, $scope, $state) {
+        'ngInject';
+        if (!$stateParams.origin || !$stateParams.destination) {
+            $state.go('app.dashboard');
+            return;
         }
-        
-    };
-    
-    this.data = [
-        {
-            "key" : "Quantity" ,
-            "bar": true,
-            "values" : [ [5,1] , [10,5] , [15, 10] , [20, 7], [25, 6] , [30, 15], [35, -25]]
-        }];
-   
+        this.origin = $stateParams.origin;
+        this.dest = $stateParams.destination;
+
+        this.$log = $log;
+        this.origin = $stateParams.origin;
+        this.dest = $stateParams.destination;
+        this.flightService = flightService;
+        this.options = {
+            chart: {
+                type: 'historicalBarChart',
+                height: 400,
+                margin: {
+                    top: 20,
+                    right: 20,
+                    bottom: 65,
+                    left: 50
+                },
+                x: function (d) { return d[0]; },
+                y: function (d) { return d[1]; },
+                showValues: true,
+                duration: 100,
+                xAxis: {
+                    axisLabel: 'X Axis',
+                    rotateLabels: 30,
+                    showMaxMin: false
+                },
+                yAxis: {
+                    axisLabel: 'Y Axis',
+                    axisLabelDistance: -10,
+                },
+                tooltip: {
+                    keyFormatter: function (d) {
+                        return d3.time.format('%x')(new Date(d));
+                    }
+                },
+                zoom: {
+                    enabled: true,
+                    scaleExtent: [1, 10],
+                    useFixedDomain: false,
+                    useNiceScale: false,
+                    horizontalOff: false,
+                    verticalOff: true,
+                    unzoomEventType: 'dblclick.zoom'
+                }
+            }
+
+        };
+
+        this.data = [
+            {
+                "key": "Quantity",
+                "bar": true,
+                "values": [[5, 1], [10, 5], [15, 10], [20, 7], [25, 6], [30, 15], [35, -25]]
+            }];
+
         let unsubscribe = $ngRedux.connect(this.mapStateToThis, actions)(this);
         $scope.$on('$destroy', unsubscribe);
 
         this.correlationPointsAvalibile = false;
-    } 
+    }
 
     $onInit() {
         this.selectFlight(this.origin, this.dest);
     }
 
-  
-    $doCheck() {
-        console.log("check");
-        
 
-        if(this.correlationPoints && !this.correlationPointsAvalibile) {
+    $doCheck() {
+
+        if (this.correlationPoints && !this.correlationPointsAvalibile) {
             this.options1 = {
                 chart: {
-                    type: 'cumulativeLineChart',
+                    type: 'lineChart',
                     height: 450,
-                    margin : {
+                    margin: {
                         top: 20,
                         right: 20,
                         bottom: 65,
                         left: 50
                     },
-                    x: function(d){return d[0];},
-                    y: function(d){return d[1];},
+                    x: function (d) { return d[0]; },
+                    y: function (d) { return d[1]; },
                     useInteractiveGuideline: true,
                     showValues: false,
                     duration: 100,
@@ -105,26 +103,26 @@ class FlightDetailsCtrl {
                     },
                     yAxis: {
                         axisLabel: 'Avarage delays in minutes',
-                        axisLabelDistance: 20,
+                        axisLabelDistance: -10
                     },
                     tooltip: {
-                        keyFormatter: function(d) {
+                        keyFormatter: function (d) {
                             return d3.time.format('%x')(new Date(d));
                         }
-                    },
-                    // forceX: [0, 3000],
-                    // forceY: [-40, 140]
-                }
-                };
-                this.data1 = [
-                    {
-                        key: "Correlation between delays and distance",
-                        values: [ [1000,1] , [10,5] , [15, 10] , [20, 7], [25, 6] , [30, 15], [35, -1000]],
-                        mean: 500
                     }
-                ];
-                this.correlationPointsAvalibile = true;
-                console.log(this.correlationPoints);
+                },
+                title: {
+                    enable: true,
+                    text: "Correlation between delays and distances"
+                }
+            };
+            this.data1 = [
+                {
+                    key: "Avarage delays in minutes",
+                    values: this.correlationPoints
+                }
+            ];
+            this.correlationPointsAvalibile = true;
         }
     }
 
@@ -134,6 +132,6 @@ class FlightDetailsCtrl {
             correlationPoints: getCorrelationPoints(state)
         }
     }
- }
- 
- export default FlightDetailsCtrl;
+}
+
+export default FlightDetailsCtrl;
